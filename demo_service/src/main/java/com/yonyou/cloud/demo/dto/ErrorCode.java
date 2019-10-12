0ee634d3049fccd5926de8ea27e2c0fc1130dd93
zhangwbin <zhangwbin@yonyou.com>
2019-10-12 10:18:34 +0800

    [MOD]优化异常，捕获spring的基本异常

--- demo_service/src/main/java/com/yonyou/cloud/demo/dto/ErrorCode.java
+++ demo_service/src/main/java/com/yonyou/cloud/demo/dto/ErrorCode.java
@@ -1,5 +1,6 @@
 package com.yonyou.cloud.demo.dto;
 
+import javax.servlet.http.HttpServletResponse;
 import lombok.Getter;
 
 public enum ErrorCode {
@@ -10,6 +11,16 @@ public enum ErrorCode {
   FileTypeError("1003", "文件类型不合法"),
   FileSizeError("1004", "文件大小不合法"),
   FileNotExist("1005", "文件不存在"),
+
+
+  NOT_FOUND(String.valueOf(HttpServletResponse.SC_NOT_FOUND), "404 Not Found"),
+  MSG_NOT_READABLE(String.valueOf(HttpServletResponse.SC_BAD_REQUEST), "Message Can't be Read"),
+  METHOD_NOT_SUPPORTED(String.valueOf(HttpServletResponse.SC_METHOD_NOT_ALLOWED), "Method Not Supported"),
+  MEDIA_TYPE_NOT_SUPPORTED(String.valueOf(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE), "Media Type Not Supported"),
+  PARAM_MISS(String.valueOf(HttpServletResponse.SC_BAD_REQUEST), "Missing Required Parameter"),
+  PARAM_TYPE_ERROR(String.valueOf(HttpServletResponse.SC_BAD_REQUEST), "Parameter Type Mismatch"),
+  PARAM_BIND_ERROR(String.valueOf(HttpServletResponse.SC_BAD_REQUEST), "Parameter Binding Error"),
+  PARAM_VALID_ERROR(String.valueOf(HttpServletResponse.SC_BAD_REQUEST), "Parameter Validation Error")
   ;
 
   @Getter
--- demo_service/src/main/java/com/yonyou/cloud/demo/exception/GlobalExceptionHandler.java
+++ demo_service/src/main/java/com/yonyou/cloud/demo/exception/GlobalExceptionHandler.java
@@ -2,22 +2,93 @@ package com.yonyou.cloud.demo.exception;
 
 import com.alibaba.fastjson.JSON;
 import com.yonyou.cloud.demo.dto.BaseResult;
+import com.yonyou.cloud.demo.dto.ErrorCode;
 import com.yonyou.cloud.demo.utils.ResultUtils;
 import java.io.FileNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+  @ExceptionHandler(MissingServletRequestParameterException.class)
+  public BaseResult handleError(MissingServletRequestParameterException e) {
+    log.warn("Missing Request Parameter", e);
+    String message = String.format("Missing Request Parameter: %s", e.getParameterName());
+    return new BaseResult(ErrorCode.PARAM_MISS, message);
+  }
 
-  // spring默认上传大小1MB 超出大小捕获异常MaxUploadSizeExceededException */
+  @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+  public BaseResult handleError(MethodArgumentTypeMismatchException e) {
+    log.warn("Method Argument Type Mismatch", e);
+    String message = String.format("Method Argument Type Mismatch: %s", e.getName());
+    return new BaseResult(ErrorCode.PARAM_TYPE_ERROR, message);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public BaseResult handleError(MethodArgumentNotValidException e) {
+    log.warn("Method Argument Not Valid", e);
+    BindingResult result = e.getBindingResult();
+    FieldError error = result.getFieldError();
+    String message = String.format("%s:%s", error.getField(), error.getDefaultMessage());
+    return new BaseResult(ErrorCode.PARAM_VALID_ERROR, message);
+  }
+
+  @ExceptionHandler(BindException.class)
+  public BaseResult handleError(BindException e) {
+    log.warn("Bind Exception", e);
+    FieldError error = e.getFieldError();
+    String message = String.format("%s:%s", error.getField(), error.getDefaultMessage());
+    return new BaseResult(ErrorCode.PARAM_BIND_ERROR, message);
+  }
+
+
+  @ExceptionHandler(NoHandlerFoundException.class)
+  public BaseResult handleError(NoHandlerFoundException e) {
+    log.error("404 Not Found", e);
+    return new BaseResult(ErrorCode.NOT_FOUND, e.getMessage());
+  }
+
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public BaseResult handleError(HttpMessageNotReadableException e) {
+    log.error("Message Not Readable", e);
+    return new BaseResult(ErrorCode.MSG_NOT_READABLE, e.getMessage());
+  }
+
+  @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+  public BaseResult handleError(HttpRequestMethodNotSupportedException e) {
+    log.error("Request Method Not Supported", e);
+    return new BaseResult(ErrorCode.METHOD_NOT_SUPPORTED, e.getMessage());
+  }
+
+  @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+  public BaseResult handleError(HttpMediaTypeNotSupportedException e) {
+    log.error("Media Type Not Supported", e);
+    return new BaseResult(ErrorCode.MEDIA_TYPE_NOT_SUPPORTED, e.getMessage());
+  }
+
+  /**
+   * 捕获异常MaxUploadSizeExceededException
+   * @param e
+   * @return
+   */
   @ExceptionHandler(MaxUploadSizeExceededException.class)
   public BaseResult handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
-    return ResultUtils.fileSizeError();
+    log.warn("上传文件大小错误", e);
+    return new BaseResult(ErrorCode.FileSizeError, e.getMessage());
   }
 
   @ExceptionHandler(FileNotFoundException.class)
